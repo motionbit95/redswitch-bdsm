8d34f660469b747265f01f748d57f4ab8cdb199a
wlsdud9973 <jyp260399@gmail.com>
2024-11-20 14:35:28 +0900

    광고 배너 onclick 추가

--- src/component/advertise.js
+++ src/component/advertise.js
@@ -1,44 +1,54 @@
 import React from "react";
 import { Carousel, Image } from "antd";
 
-const AdCarousel = () => (
-  <Carousel autoplay>
-    <div className="center">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_14RWF45.jpg?alt=media&token=c305da34-d49c-481e-865b-68cc2b810bc9"
-        style={{ width: "100%", height: "auto", maxHeight: "160px" }}
-      />
-    </div>
-    <div className="center">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_ASDFASDF.jpg?alt=media&token=646c40df-e48f-4a28-a968-57c78c01c36c"
-        style={{ width: "100%", height: "auto", maxHeight: "160px" }}
-      />
-    </div>
-    <div className="center">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241012_%EC%A0%9C%EB%AA%A9%EC%9D%84%20%EC%9E%85%EB%A0%A5%ED%95%B4%EC%A3%BC%EC%84%B8%EC%9A%94_-001%20(22).jpg?alt=media&token=3cc87a61-8e38-463c-938b-cdb43f4709a7"
-        style={{ width: "100%", height: "auto", maxHeight: "160px" }}
-      />
-    </div>
-    <div className="center">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241012_%EC%A0%9C%EB%AA%A9%EC%9D%84%20%EC%9E%85%EB%A0%A5%ED%95%B4%EC%A3%BC%EC%84%B8%EC%9A%94_-001%20(22).jpg?alt=media&token=888800e4-5de5-41df-bbb2-6a34933ad9ed"
-        style={{ width: "100%", height: "auto", maxHeight: "160px" }}
-      />
-    </div>
-    <div className="center">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241016_KakaoTalk_20241016_173949491.png?alt=media&token=6737880a-d250-4ffd-aec2-8a7d3829536d"
-        style={{ width: "100%", height: "auto", maxHeight: "160px" }}
-      />
-    </div>
-    <div className="center">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_ASA3232.jpg?alt=media&token=8d0ff71d-4841-40d0-b624-7714f00108e3"
-        style={{ width: "100%", height: "auto", maxHeight: "160px" }}
-      />
-    </div>
-  </Carousel>
-);
+const AdCarousel = () => {
+  const Images = [
+    {
+      banner_image:
+        "https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_ASDFASDF.jpg?alt=media&token=646c40df-e48f-4a28-a968-57c78c01c36c",
+      banner_site: "http://specialnight.co.kr/",
+    },
+    {
+      banner_image:
+        "https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_14RWF45.jpg?alt=media&token=c305da34-d49c-481e-865b-68cc2b810bc9",
+      banner_site: "http://redswitch.kr/",
+    },
+    {
+      banner_image:
+        "https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_ASDFASDF.jpg?alt=media&token=646c40df-e48f-4a28-a968-57c78c01c36c",
+      banner_site: "http://taeryun.kr/",
+    },
+    {
+      banner_image:
+        "https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_14RWF45.jpg?alt=media&token=c305da34-d49c-481e-865b-68cc2b810bc9",
+      banner_site: "http://specialnight.co.kr/",
+    },
+    {
+      banner_image:
+        "https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_ASDFASDF.jpg?alt=media&token=646c40df-e48f-4a28-a968-57c78c01c36c",
+      banner_site: "http://redswitch.kr/",
+    },
+    {
+      banner_image:
+        "https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/banner_image%2F20241011_14RWF45.jpg?alt=media&token=c305da34-d49c-481e-865b-68cc2b810bc9",
+      banner_site: "http://taeryun.kr/",
+    },
+  ];
+  return (
+    <Carousel autoplay>
+      {Images.map((image, index) => (
+        <div className="center" key={index}>
+          <Image
+            preview={false}
+            onClick={() => {
+              window.open(image.banner_site, "_blank");
+            }}
+            src={image.banner_image}
+            style={{ width: "100%", height: "auto", maxHeight: "160px" }}
+          />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
 export default AdCarousel;
